11c842d874454aed82261bfac053262521d822d1
ManojKumar R <127740667+Manoj1904@users.noreply.github.com>
2025-02-06 10:27:59 +0530

    Update deploy.sh

--- Jenkin-main/deploy.sh
+++ Jenkin-main/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker stop website-container || true
 docker rm website-container || true
-docker pull your-username/capstone:latest
-docker run -d -p 8080:80 --name website-container your-username/capstone:latest
+docker pull manoj1904/devops:latest
+docker run -d -p 8080:80 --name website-container manoj1904/devops:latest
